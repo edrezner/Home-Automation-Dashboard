27a1e5cda849c5d48ee0ac269b361cb2cfea82a5
pvlln <pv823@nyu.edu>
2023-07-06 20:11:02 -0400

    created actions and reducer functions

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,1 +1,2 @@
 import React, { createContext, useContext } from "react";
+
---
+++ client/src/utils/actions.js
@@ -0,0 +1,17 @@
+// To add, update, and remove homes
+export const ADD_HOME = "ADD_HOME";
+export const UPDATE_HOME = "UPDATE_HOME";
+export const REMOVE_HOME = "REMOVE_HOME";
+
+// To add, update, and remove rooms
+export const ADD_ROOM = "ADD_ROOM";
+export const UPDATE_ROOM = "UPDATE_ROOM";
+export const REMOVE_ROOM = "REMOVE_ROOM";
+
+// To add, update, and remove devices
+export const ADD_DEVICE = "ADD_DEVICE";
+export const UPDATE_DEVICE = "UPDATE_DEVICE";
+export const REMOVE_DEVICE = "REMOVE_DEVICE";
+
+// To update a device's settings
+export const UPDATE_SETTINGS = "UPDATE_SETTINGS";
